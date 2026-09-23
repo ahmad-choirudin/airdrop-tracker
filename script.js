48d51d01c7f6cb9a527a5bbed22d252e6f42fcb9
Ahmad Choirudin <42575831+ahmad-choirudin@users.noreply.github.com>
2026-09-23 21:32:54 +0700

    Implement data fetching and display for airdrop projects
    Fetch and display airdrop project data from JSON.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Airdrop Tracker is running!");
+fetch("data.json")
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById("airdrop-list");
+
+    data.forEach(project => {
+      const card = document.createElement("div");
+
+      card.innerHTML = `
+        <h2>${project.name}</h2>
+        <p>Type: ${project.type}</p>
+        <p>Status: ${project.status}</p>
+        <p>Chain: ${project.chain}</p>
+        <p>Reward: ${project.reward}</p>
+      `;
+
+      container.appendChild(card);
+    });
+  });
